b34f107563002bae68e827871fe38ce7a0348fba
Nikita Zasimuk <n.zasimuk@aiolane.com>
2022-02-06 18:31:57 +0300

    feat: load user email

--- src/NetlifyGithubLogin.tsx
+++ src/NetlifyGithubLogin.tsx
@@ -1,48 +1,77 @@
 import netlify from 'netlify-auth-providers';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { GraphQLClient } from 'graphql-request'
 
+interface GithubOAuthResponse {
+  provider: "github" | "gitlab" | "bitbucket"
+  token: string
+}
+
 async function authWithGitHub() {
-  return new Promise((resolve, reject) => {
+  return new Promise<GithubOAuthResponse>((resolve, reject) => {
     const authenticator = new netlify({
       site_id: 'ba38890c-5f4b-4b1f-a311-2975a400303e',
     })
+
     authenticator.authenticate(
       {provider: 'github', scope: 'public_repo,read:org,read:user'},
-      function(err, data) {
-        if (err) {
-          reject(err)
-        }
-        resolve(data)
-      },
+      (err, data) => err ? reject(err) : resolve(data),
     )
   })
 }
 
-function getClient(token: string) {
-  const headers = {Authorization: `bearer ${token}`}
-  return new GraphQLClient('https://api.github.com/graphql', {headers})
+function getClient(token: string): GraphQLClient {
+  const headers = { Authorization: `bearer ${token}` }
+  return new GraphQLClient('https://api.github.com/graphql', { headers })
+}
+
+async function loadGitHubUserEmails(token: string) {
+  return await fetch("https://api.github.com/user/emails", {
+    headers: {
+      Accept: "application/vnd.github.v3+json",
+      Authorization: `token ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => JSON.stringify(response));
 }
 
 export const NetlifyGithubLogin = () => {
   const [error, setError] = useState<null | any>(null)
-  const [client, setClient] = useState(null)
+  const [client, setClient] = useState<null | GraphQLClient>(null)
+
+  useEffect(() => {
+    if (!client) {
+      const currentGithubToken = localStorage.getItem('GITHUB_TOKEN')
+      if (currentGithubToken) {
+        const newClient = getClient(currentGithubToken)
+        setClient(newClient)
+      }
+    }
+  }, [client])
 
   const handleLoginClick = async () => {
-    const data = await authWithGitHub().catch(error => {
+    try {
+      const data = await authWithGitHub()
+      console.log({ data })
+      localStorage.setItem('GITHUB_TOKEN', data.token)
+
+      const email = await loadGitHubUserEmails(data.token)
+      console.log(email)
+      const newClient = getClient(data.token)
+      console.log(newClient)
+
+      setClient(newClient)
+    } catch (error) {
       console.log('Oh no', error)
       setError({ error })
-    })
-    console.log({ data })
-    // @ts-ignore
-    localStorage.setItem('GITHUB_TOKEN', data.token)
-    // @ts-ignore
-    setClient(getClient(data.token))
+    }
   }
 
-  if (client) return <Redirect to="/" />
-  else if (error) return (
+  // if (client) return <Redirect to="/" />
+  // else if (error) return (
+  if (error) return (
     <div>
       Oh no! Error! <pre>{JSON.stringify(error, null, 2)}</pre>
     </div>
@@ -58,23 +87,4 @@ export const NetlifyGithubLogin = () => {
         <Route path="*" render={() => <Redirect to="/login" />} />
       </>
     )
-  // return <>
-  //   client ? (
-  //   <Redirect to="/" />
-  //   ) : error ? (
-  //   <div>
-  //     Oh no! Error! <pre>{JSON.stringify(error, null, 2)}</pre>
-  //   </div>
-  //   ) : (
-  //   <>
-  //     <Route path="/login" render={() => (
-  //       <div>
-  //         You have no client!
-  //         <button onClick={handleLoginClick}>Sign In Here!</button>
-  //       </div>
-  //     )} />
-  //     <Route path="*" render={() => <Redirect to="/login" />} />
-  //   </>
-  //   )
-  // </>
 }
