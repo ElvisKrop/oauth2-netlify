a67bd30168b89579609346255c5aff506f680556
Nikita Zasimuk <n.zasimuk@aiolane.com>
2022-02-17 15:51:25 +0300

    feat: matic faucet

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,16 @@ import { BrowserRouter as Router, Link, Redirect, Route } from 'react-router-dom
 import NetlifyOAuthWrapper from './NetlifyOAuthWrapper'
 import { NetlifyOAuthProvider } from './enums'
 import { UserProfile } from './types'
+import HomePage from './HomePage'
+import { allProviders } from './constants'
 
 const App = () => {
+  const acceptedProviders = (process.env?.REACT_APP_ACCEPTED_PROVIDERS?.trim()
+    ?.toLowerCase()
+    .split(',')
+    .filter((provider) => allProviders.includes(provider as NetlifyOAuthProvider)) ||
+    []) as NetlifyOAuthProvider[]
+
   const handleLogin = useCallback((provider: NetlifyOAuthProvider, profile: UserProfile) => {
     console.log(provider, profile)
     return Promise.resolve()
@@ -14,12 +22,11 @@ const App = () => {
     <Router>
       <NetlifyOAuthWrapper
         handleLogin={handleLogin}
-        acceptedProviders={[NetlifyOAuthProvider.github, NetlifyOAuthProvider.gitlab]}
+        acceptedProviders={acceptedProviders}
         apiId={process.env?.REACT_APP_NETLIFY_API_ID}
       >
         <Route exact path="/home">
-          <h1>Root Page</h1>
-          <Link to="/secondary">go to secondary page</Link>
+          <HomePage />
         </Route>
         <Route exact path="/secondary">
           <h1>Secondary Page</h1>
